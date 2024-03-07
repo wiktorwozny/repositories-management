0980339c82e4251fc4cb484dc740feff83cdb287
wiktorwozny <wikivip10@gmail.com>
2024-03-07 19:45:54 +0100

    initial

---
+++ src/main/java/pl/edu/agh/repomanagement/RepositoriesManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.repomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoriesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoriesManagementApplication.class, args);
+    }
+
+}
