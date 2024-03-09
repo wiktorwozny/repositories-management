c46339c7560b7987b169c1e06f37cdd89c7bf4c5
Green0x0y <laurawiktor44@gmail.com>
2024-03-09 20:18:29 +0100

    remove repositories for now

--- src/main/java/pl/edu/agh/repomanagement/backend/repositories/RepositoryRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.repomanagement.backend.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import pl.edu.agh.repomanagement.backend.models.Repository;
-
-public interface RepositoryRepository extends MongoRepository<Repository, String> {
-}
