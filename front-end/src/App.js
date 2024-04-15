3f72d7d5fb75cbcf34c5b59330560398b9f0148f
siendobry <siendobry@gmail.com>
2024-04-16 01:50:00 +0200

    Add basic routing and create views for authentication

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,10 +4,24 @@ import { connect } from "react-redux";
 
 import { useSelector, useDispatch } from "react-redux";
 import { fetchWorkspaceList } from "./store/slices/workspaceSlice";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AxiosInterceptor } from "./api/client";
+import Authentication from "./pages/Authentication";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  return (
+      <Router>
+          <AxiosInterceptor>
+              <Routes>
+                  <Route path="/login" element={ <Authentication isSignedUp={ true } /> } />
+                  <Route path="/signup" element={ <Authentication iSignedUp={ false } /> } />
+                  <Route path="/dashboard" element={ <Dashboard /> } />
+                  <Route path="*" element={ <Authentication signedUp={ true } /> } />
+              </Routes>
+          </AxiosInterceptor>
+      </Router>
+  );
 }
 
 export default connect()(App);
---
+++ front-end/src/pages/Authentication.js
@@ -0,0 +1,77 @@
+import {connect, useDispatch} from "react-redux";
+
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Button } from "@mui/material";
+
+import LoginForm from "../components/authentication/LoginForm";
+import SignupForm from "../components/authentication/SignupForm";
+import {useNavigate} from "react-router-dom";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 100%;
+`;
+
+const TopBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  padding: 1rem;
+`;
+
+const Controls = styled.div`
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+`;
+
+function Authentication({ isSignedUp }) {
+    const navigate = useNavigate();
+
+    const handleSignedUp = () => {
+        navigate("/login");
+    };
+
+    const handleNotSignedUp = () => {
+        navigate("/signup");
+    };
+
+    return (
+        <Wrapper>
+            <TopBar>
+                <h1>Dashboard</h1>
+                <Controls>
+                    <Button variant="outlined" onClick={handleSignedUp}>
+                        Log in
+                    </Button>
+                    <Button variant="outlined" onClick={handleNotSignedUp}>
+                        Sign up
+                    </Button>
+                </Controls>
+            </TopBar>
+
+            <Form>
+                {isSignedUp ? (
+                    <LoginForm />
+                ) : (
+                    <SignupForm />
+                )}
+            </Form>
+        </Wrapper>
+    );
+}
+
+export default Authentication;
