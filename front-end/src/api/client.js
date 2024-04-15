33df48306b7a692fe91b630c71c8d1ef7ea284fd
siendobry <siendobry@gmail.com>
2024-04-16 01:51:37 +0200

    Add automatic redirection to login view if user is not authenticated

--- front-end/src/api/client.js
+++ front-end/src/api/client.js
@@ -1,10 +1,42 @@
 /// create and export axios client
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
 
-export default axios.create({
+const client = axios.create({
+  withCredentials: true,
   baseURL: "http://localhost:8080",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Credentials": true,
   },
 });
+
+function AxiosInterceptor({ children }) {
+    const [isSet, setIsSet] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interceptor = client.interceptors.response.use(
+            response => {
+                return response;
+            },
+            error => {
+                if (error.response.status === 401) {
+                    console.log("401")
+                    navigate("/login");
+                }
+                return Promise.reject(error);
+            }
+        )
+
+        setIsSet(true);
+
+        return () => client.interceptors.response.eject(interceptor);
+    }, [navigate]);
+
+    return isSet && children;
+}
+
+export default client;
+export { AxiosInterceptor };
